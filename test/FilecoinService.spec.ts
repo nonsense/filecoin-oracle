4d46e1b93a061a4bd14635afffac895a8d8bb37d
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-02-03 20:46:12 +0100

    more tests

--- test/FilecoinService.spec.ts
+++ test/FilecoinService.spec.ts
@@ -1,7 +1,7 @@
 import chai, { expect } from 'chai'
 import { solidity, MockProvider, deployContract } from 'ethereum-waffle'
 import { Contract, BigNumber, constants } from 'ethers'
-import BalanceTree from '../src/balance-tree'
+import CidsTree from '../src/cids-tree'
 
 import FilecoinService from '../artifacts/contracts/FilecoinService.sol/FilecoinService.json'
 
@@ -48,6 +48,25 @@ describe('FilecoinService', () => {
       )).to.be.revertedWith(
         'invalid proof'
       )
+    })
+
+    describe('two account tree', () => {
+      let filecoinService: Contract
+      let tree: CidsTree
+      beforeEach('deploy', async () => {
+        tree = new CidsTree([
+          {  dataCid: "datacid1234", pieceCid: "piececid1234", dealId: BigNumber.from(150505), provider: "fprovider1", startEpoch: BigNumber.from(10), endEpoch: BigNumber.from(2000), signedEpoch: BigNumber.from(50) },
+          {  dataCid: "datacid2345", pieceCid: "piececid2345", dealId: BigNumber.from(267775), provider: "fprovider2", startEpoch: BigNumber.from(100), endEpoch: BigNumber.from(4000), signedEpoch: BigNumber.from(250) },
+        ])
+        filecoinService = await deployContract(wallet0, FilecoinService, [tree.getHexRoot(), 10000], overrides)
+      })
+
+      it('successful proof', async () => {
+        const proof0 = tree.getProof("datacid1234", "piececid1234", BigNumber.from(150505), "fprovider1", BigNumber.from(10), BigNumber.from(2000), BigNumber.from(50))
+        await expect(filecoinService.submitProof("datacid1234", "piececid1234", BigNumber.from(150505), "fprovider1", BigNumber.from(10), BigNumber.from(2000), BigNumber.from(50), proof0, overrides))
+          .to.emit(filecoinService, 'StoredCid')
+          //.withArgs(0, wallet0.address, 100)
+      })
     })
   })
 });
