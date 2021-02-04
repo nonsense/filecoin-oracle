0b7d3927bb82f3d9f6c8e395c125318e230d3cb2
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-02-04 19:42:06 +0100

    add read-only verifyProof func

--- test/FilecoinService.spec.ts
+++ test/FilecoinService.spec.ts
@@ -36,18 +36,7 @@ describe('FilecoinService', () => {
   describe('#submitProof', () => {
     it('fails for empty proof', async () => {
       const filecoinService = await deployContract(wallet0, FilecoinService, [ZERO_BYTES32, 100000], overrides)
-      await expect(filecoinService.submitProof(
-    "dataCid",
-    "pieceCid",
-    123,
-    "provider",
-    10,
-    100,
-    50,
-    []
-      )).to.be.revertedWith(
-        'invalid proof'
-      )
+      await expect(filecoinService.submitProof("dataCid", "pieceCid", 123, "provider", 10, 100, 50, [])).to.be.revertedWith('invalid proof')
     })
 
     describe('two cids tree', () => {
@@ -63,17 +52,21 @@ describe('FilecoinService', () => {
 
       it('successful and broken proof', async () => {
         var proof0 = tree.getProof("datacid1234", "piececid1234", BigNumber.from(150505), "fprovider1", BigNumber.from(10), BigNumber.from(2000), BigNumber.from(50))
-        //for (var item of proof0) {
-          //console.log(item)
-        //}
         await expect(filecoinService.submitProof("datacid1234", "piececid1234", BigNumber.from(150505), "fprovider1", BigNumber.from(10), BigNumber.from(2000), BigNumber.from(50), proof0, overrides))
           .to.emit(filecoinService, 'StoredCid')
-          //.withArgs(0, wallet0.address, 100)
+
+        var resCorrect = await filecoinService.verifyProof("datacid1234", "piececid1234", BigNumber.from(150505), "fprovider1", BigNumber.from(10), BigNumber.from(2000), BigNumber.from(50), proof0, overrides);
+
+        expect(resCorrect).to.eq(true)
 
         var brokenProof = proof0
         brokenProof[0] = "0xe476bc2413fc2aff9f04a6ad695bc158b5420a6d98f6691cfb7ce9a9ec2cdcf8"
         await expect(filecoinService.submitProof("datacid1234", "piececid1234", BigNumber.from(150505), "fprovider1", BigNumber.from(10), BigNumber.from(2000), BigNumber.from(50), brokenProof, overrides))
           .to.be.revertedWith("invalid proof")
+
+        var resBroken = await filecoinService.verifyProof("datacid1234", "piececid1234", BigNumber.from(150505), "fprovider1", BigNumber.from(10), BigNumber.from(2000), BigNumber.from(50), brokenProof, overrides);
+
+        expect(resBroken).to.eq(false)
       })
     })
   })
