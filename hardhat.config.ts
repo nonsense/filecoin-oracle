a8db58484f404ec4dc27ad815de00944b2c7bc43
ZX <16908497+zixuanzh@users.noreply.github.com>
2021-02-05 03:58:58 -0800

    Add deployment script (#3)
    * add deployment script
    
    * update gitignore
    
    Co-authored-by: zixuanzh <zixuanzh@users.noreply.github.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,16 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 
+// Go to https://infura.io/ and create a new project
+// Replace this with your Infura project ID
+const INFURA_PROJECT_ID = "YOUR INFURA PROJECT ID";
+
+// Replace this private key with your rinkeby account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const RINKEBY_PRIVATE_KEY = "YOUR RINKEBY PRIVATE KEY";
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
@@ -13,4 +23,10 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 
 export default {
   solidity: "0.7.6",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`]
+    }
+  }
 };
